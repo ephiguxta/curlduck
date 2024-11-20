5164f0fc189b6727a1f67e1b8d61830cd5258f5b
Benjamin Henrion <zoobab@gmail.com>
2024-11-20 13:33:12 +0100

    Update curlduck.sh: add check to exit if curl jq and others are not installed
    Update curlduck.sh: add check to exit if curl jq and others are not installed

--- curlduck.sh
+++ curlduck.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+check_commands() {
+  for cmd in "$@"; do
+    if command -v "$cmd" &> /dev/null; then
+      echo "$cmd is installed and available."
+    else
+      echo "$cmd is not installed or not available in the system's PATH."
+    fi
+  done
+}
+check_commands curl jq sed tr awk grep echo
+
 DUCKDUCKGO_STATUS_URL='https://duckduckgo.com/duckchat/v1/status'
 DUCKDUCKGO_CHAT_URL='https://duckduckgo.com/duckchat/v1/chat'
 VQD_RESPONSE=$(curl -s -D - -H "x-vqd-accept: 1" $DUCKDUCKGO_STATUS_URL)
